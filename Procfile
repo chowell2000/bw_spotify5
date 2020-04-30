3df9764362e0d14b4c8b382f502a1c604cf1604b
chowell2000 <chowell2000@gmail.com>
2020-04-30 04:52:17 -0700

    working model!

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn spotify5:app
--- Spotify5.py
+++ Spotify5.py
@@ -1,42 +1,65 @@
 """Reddit prediction model Flask App"""
 
-from flask import Flask, render_template, request
+from flask import Flask, jsonify, render_template, request
+
+import json
 import pickle
 import pandas as pd
+import sklearn
+from urllib.request import urlopen
 
+    
 app = Flask(__name__)
+pickled = "https://github.com/chowell2000/bw_spotify5/raw/master/model_1.pkl"
+pickled_scaler = "https://github.com/chowell2000/bw_spotify5/raw/master/scaler_1.pkl"
+pickled_df = "https://github.com/chowell2000/bw_spotify5/raw/master/df.pkl"
+
+model = pickle.load(urlopen(pickled))
+scaler = pickle.load(urlopen(pickled_scaler))
+lookup = pd.read_pickle(pickled_df)
+
+cols = ['acousticness', 'danceability', 'energy', 'instrumentalness', 'liveness', 'loudness', 'speechiness', 'tempo', 'valence']
 
 @app.route("/")
 def hello():
     return render_template('home.html')
 
-@app.route("/querytest", methods=['GET'])
+@app.route("/querytest", methods=['POST'])
 def query_test():
-    x = request.args.get("text")
-    return render_template('predict.html', forum = x)
+    x = request.get_json(force=True)
+    return x
+    # render_template('predict.html', forum = x)
 
 
+@app.route("/predict", methods=['POST'])
+def predict(data=None):
+    try:
+        if request.method == 'POST':
+            data = request.get_json(force=True)
+    except KeyError:
+        return ('''Bad request: value missing''')
+    else:
+        def model_prediction(data):
+            data_df = pd.DataFrame(columns=cols)
+            for i in range(len(data)):
+                temp = pd.DataFrame.from_records(data[i], index=[i])
+                data_df = data_df.append(temp[cols])
 
-@app.route("/predict", methods=['GET', 'POST'] )
-def predict():
-    # text = 'I want to play the new call of duty'
-    text = request.args.get("text")
-    # nn = pickle.load(open('model.pkl', 'rb'))
-    # vect = pickle.load(open('countvectorizer.pkl', 'rb'))
-    # subset = pd.read_pickle("./dataset.pkl")
-    # new = vect.transform([text])
-    
-    # x = nn.kneighbors(new.todense())
-    
-    # y = x[1][0][0]
-    # z = subset.iloc[y][0]
-    
-    return render_template('predict.html', forum=z)
+            data_scaled = pd.DataFrame(scaler.transform(data_df), columns=cols)
+            
+            
+            predict = model.kneighbors(data_scaled, return_distance=False)
 
+            # return predicter[0]
+            return predict
+        prediction = model_prediction(data)
+        result = []
+        for i in range(len(prediction[0])):
+            resi = lookup[cols + ['track_id', 'track_name']].iloc[prediction[0][i]].to_dict()
+            result.append(resi)
+        return str(result)
 
 
 @app.route("/about")
 def preds():
     return render_template('about.html')
-
-# ,method = POST
